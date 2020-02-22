20a2a675143142e37ccf2bd7965db095959e0607
G3bE <nik.wipper@gmx.de>
2020-02-22 23:00:44 +1000

    Update type names

--- include/Preprocessor/Types.h
+++ include/Preprocessor/Types.h
@@ -8,16 +8,16 @@
 #define var auto
 
 typedef int8_t                   i8;
-typedef uint8_t                 ui8;
+typedef uint8_t                  u8;
 
 typedef int16_t                 i16;
-typedef uint16_t               ui16;
+typedef uint16_t                u16;
 
 typedef int32_t                 i32;
-typedef uint32_t               ui32;
+typedef uint32_t                u32;
 
 typedef int64_t                 i64;
-typedef uint64_t               ui64;
+typedef uint64_t                u64;
 
 typedef const char *         string;
 
